2176f6544d073720c1d3622572164486dbcd6b0e
Ahmed Onawale <ahmedonawale@gmail.com>
2017-04-09 17:24:58 +0100

    Update Record protocol requirements. Create JSONInitializable protocol. Remove Serializable protocol.

--- Sources/SwiftDataStore/Record.swift
+++ Sources/SwiftDataStore/Record.swift
@@ -6,6 +6,24 @@
 //  Copyright © 2017 Ahmed Onawale. All rights reserved.
 //
 
+/// This protocol outlines the requirement of a type that
+/// can be converted to a `JSON`
+public protocol JSONRepresentable {
+    /// Call this method to get a `JSON` representation of `Self`
+    /// - Returns: The json representation of `Self`
+    func toJSON() -> JSON
+}
+
+/// This protocol outlines the requirement of a type that
+/// can created from a `JSON`
+public protocol JSONInitializable {
+    /// Required initializer
+    /// - Parameters:
+    ///  - id: An ID struct uniquely identifying `Self`
+    ///  - hash: A dictionary of [String: Any]
+    init(id: ID, hash: JSON) throws
+}
+
 /// Unique Record identifier.
 /// Conforms to Hashable protocol
 public struct ID: Hashable {
@@ -31,6 +49,13 @@ public struct ID: Hashable {
     }
 }
 
+/// ID CustomStringConvertible conformance
+extension ID: CustomStringConvertible {
+    public var description: String {
+        return value
+    }
+}
+
 /// ID Equatable protocol conformance
 public func ==(lhs: ID, rhs: ID) -> Bool {
     return lhs.value == rhs.value
@@ -39,49 +64,39 @@ public func ==(lhs: ID, rhs: ID) -> Bool {
 /// Record
 /// This protocol outline requirements for
 /// a type that can be managed by the `Store`
-public protocol Record: Serializable, Normalizable {
+public protocol Record: JSONRepresentable, JSONInitializable {
     /// A unique identifier for a `Self`
-    var id: ID { get }
+    var id: ID? { get }
     /// The type class to be used to make network request
     /// for `Self`. Default value is the `JSONAdapter`.
-    static var adapterClass: AdapterType.Type { get }
+    static var adapterType: Adapter.Type { get }
     /// The type class to be used to serializer and normalize
     /// `Self`. Default value is the `JSONSerializer`.
-    static var serializerClass: SerializerType.Type { get }
+    static var serializerType: Serializer.Type { get }
 }
 
 /// Default Record properties
 public extension Record {
-    static var adapterClass: AdapterType.Type {
-        return Adapter.self
+    static var adapterType: Adapter.Type {
+        return RESTAdapter.self
     }
     
-    static var serializerClass: SerializerType.Type {
+    static var serializerType: Serializer.Type {
         return JSONSerializer.self
+    }
+    
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.id != nil && lhs.id == rhs.id
     }
 }
 
-func ==<T: Record>(lhs: T, rhs: T) -> Bool {
-    return lhs.id == rhs.id
-}
-
-///// This protocol outline requirements for
-///// a type that can be represented as a JSON
-//public protocol JSONRepresentable {
-//    var JSONRepresentation: JSON { get }
-//}
-
-/// This protocol outline requirements for
-/// a type that can be serialized
-public protocol Serializable { }
-
-/// Default Serializable properties
-public extension Serializable {
+/// Default JSONRepresentable method
+public extension JSONRepresentable {
     func toJSON() -> JSON {
         var json = JSON()
         
         for case let (label?, value) in Mirror(reflecting: self).children {
-            if let value = value as? Serializable {
+            if let value = value as? JSONRepresentable {
                 json[label] = value.toJSON()
             } else if let value = value as? NSObject {
                 json[label] = value
@@ -90,15 +105,4 @@ public extension Serializable {
         
         return json
     }
-}
-
-/// This protocol outline requirements for
-/// a type that can be normalized
-public protocol Normalizable {
-    /// Failable Required initializer
-    /// - Parameters:
-    ///  - id: An ID struct uniquely identifying `Self`
-    ///  - hash: A dictionary of [String: Any] containing
-    ///    properties and values of `Self`
-    init?(id: ID, hash: JSON)
 }
