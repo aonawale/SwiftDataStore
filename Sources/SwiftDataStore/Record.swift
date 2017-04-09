c2903e1b760c548361ca8989a23e81760d50e22d
Ahmed Onawale <ahmedonawale@gmail.com>
2017-04-09 23:54:31 +0100

    Remove default toJSON implementation

--- Sources/SwiftDataStore/Record.swift
+++ Sources/SwiftDataStore/Record.swift
@@ -6,24 +6,6 @@
 //  Copyright © 2017 Ahmed Onawale. All rights reserved.
 //
 
-/// This protocol outlines the requirement of a type that
-/// can be converted to a `JSON`
-public protocol JSONRepresentable {
-    /// Call this method to get a `JSON` representation of `Self`
-    /// - Returns: The json representation of `Self`
-    func toJSON() -> JSON
-}
-
-/// This protocol outlines the requirement of a type that
-/// can created from a `JSON`
-public protocol JSONInitializable {
-    /// Required initializer
-    /// - Parameters:
-    ///  - id: An ID struct uniquely identifying `Self`
-    ///  - hash: A dictionary of [String: Any]
-    init(id: ID, hash: JSON) throws
-}
-
 /// Unique Record identifier.
 /// Conforms to Hashable protocol
 public struct ID: Hashable {
@@ -61,6 +43,24 @@ public func ==(lhs: ID, rhs: ID) -> Bool {
     return lhs.value == rhs.value
 }
 
+/// This protocol outlines the requirement of a type that
+/// can be converted to a `JSON`
+public protocol JSONRepresentable {
+    /// Call this method to get a `JSON` representation of `Self`
+    /// - Returns: The json representation of `Self`
+    func toJSON() -> JSON
+}
+
+/// This protocol outlines the requirement of a type that
+/// can created from a `JSON`
+public protocol JSONInitializable {
+    /// Required initializer
+    /// - Parameters:
+    ///  - id: An ID struct uniquely identifying `Self`
+    ///  - hash: A dictionary of [String: Any]
+    init(id: ID, hash: JSON) throws
+}
+
 /// Record
 /// This protocol outline requirements for
 /// a type that can be managed by the `Store`
@@ -68,7 +68,7 @@ public protocol Record: JSONRepresentable, JSONInitializable {
     /// A unique identifier for a `Self`
     var id: ID? { get }
     /// The type class to be used to make network request
-    /// for `Self`. Default value is the `JSONAdapter`.
+    /// for `Self`. Default value is the `RESTAdapter`.
     static var adapterType: Adapter.Type { get }
     /// The type class to be used to serializer and normalize
     /// `Self`. Default value is the `JSONSerializer`.
@@ -87,22 +87,5 @@ public extension Record {
     
     static func ==(lhs: Self, rhs: Self) -> Bool {
         return lhs.id != nil && lhs.id == rhs.id
-    }
-}
-
-/// Default JSONRepresentable method
-public extension JSONRepresentable {
-    func toJSON() -> JSON {
-        var json = JSON()
-        
-        for case let (label?, value) in Mirror(reflecting: self).children {
-            if let value = value as? JSONRepresentable {
-                json[label] = value.toJSON()
-            } else if let value = value as? NSObject {
-                json[label] = value
-            }
-        }
-        
-        return json
     }
 }
